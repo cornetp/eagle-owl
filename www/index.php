22772e4d19aadfaa870f04871bcd378b2b4cddfe
Philippe Cornet <phil.cornet@gmail.com>
2012-06-19 14:19:17 +0200

    update index.php to use the stat_db

--- www/index.php
+++ www/index.php
@@ -107,7 +107,92 @@ function get_data($db, $year=0, $month=0, $day=0)
   return $arr;
 }
 
-$db = new SQLite3('be.db');
+function get_stat_data($db, $year=0, $month=0, $day=0)
+{
+  $i = 0;
+  $unit = "year";
+  $table = "energy_year_stat";
+  if($year){
+    $unit = "month";
+    $table = "energy_month_stat";
+  }
+  if($month){
+    $unit = "day";
+    $table = "energy_day_stat";
+  }
+  if($day){
+    $unit = "hour";
+    $table = "energy_hour_stat";
+  }
+
+  $req = "SELECT ";
+  $req.= "$unit, ";
+  $req.= "kwh_total, kwh_week_total, kwh_weekend_total FROM ".$table." ";
+  if($unit <> "year"){
+    $req.= "WHERE ";
+    if($year)  $req.= "year = \"$year\" ";
+    if($month) $req.= "AND month = \"$month\" ";
+    if($day)   $req.= "AND day = \"$day\" ";
+  }
+  $req.= ";";
+
+//  echo "<br/>$req<br/><br/>";
+  $result = $db->query($req);
+  $arr = array();
+  while ($res = $result->fetchArray())
+  {
+    if($unit == "month")
+      $arr[$i] = array( 0 => month_to_string($res[0]), 
+                        1 => $res[1],
+                        2 => $res[2],
+                        3 => $res[3] );
+    else
+      $arr[$i] = array( 0 => "$res[0]",
+                        1 => $res[1],
+                        2 => $res[2],
+                        3 => $res[3] );
+                        
+    $i++;
+  }
+  return $arr;
+}
+
+function get_weekend_data($db, $year=0, $month=0, $day=0)
+{
+  $i = 0;
+  if($year)
+  {
+      $table = "energy_year_stat";
+    if($month)
+      $table = "energy_month_stat";
+    if($day)
+      $table = "energy_day_stat";
+    
+    $req = "SELECT kwh_week_total, kwh_weekend_total FROM ".$table;
+    $req.= " WHERE ";
+    if($year)  $req.= "year = \"$year\" ";
+    if($month) $req.= "AND month = \"$month\" ";
+    if($day)   $req.= "AND day = \"$day\" ";
+  }
+  else
+    $req = "SELECT sum(kwh_week_total), sum(kwh_weekend_total) FROM energy_year_stat";
+
+  $req.= ";";
+  
+//  echo "<br/>$req<br/><br/>";
+  $result = $db->query($req);
+  $arr = array();
+  $res = $result->fetchArray();
+//  {
+    $arr[0] = array( 0 => 'normal', 1 => $res[0]);
+    $arr[1] = array( 0 => 'night & weekend', 1 => $res[1]);
+//    $i++;
+//  }
+  return $arr;
+}
+
+$db = new SQLite3('/home/cornetp/vagrant/lucid32/eagle-owl/src/db/eagleowl.db');
+$stat_db = new SQLite3('/home/cornetp/vagrant/lucid32/eagle-owl/src/db/eagleowl_stat.db');
 
 $year = 0;
 $month= 0;
@@ -135,7 +220,15 @@ if(isset($_GET['day']))
   $axis_x_name = 'hour';
 }
 
-$data = get_data($db, $year, $month, $day);
+if($stat_db)
+{
+  $data = get_stat_data($stat_db, $year, $month, $day);
+  $wedata = get_weekend_data($stat_db, $year, $month, $day);
+}  
+else
+{
+  $data = get_data($db, $year, $month, $day);
+}
 
 // Following lines are to select a valid date in the calendar
 if(!$year) $year = date('Y');
@@ -170,24 +263,62 @@ function draw_chart(type, title, axis_x_name)
 //myChart.setBarValuesColor('#AAAAFF');
 //myChart.setBarValuesFontSize(10);
 //myChart.setBarValuesDecimals(2);
-  myChart.setAxisPaddingTop(60);
-  myChart.setAxisPaddingRight(20);
-  myChart.setAxisPaddingLeft(60);
+//  myChart.setAxisPaddingTop(60);
+//  myChart.setAxisPaddingRight(20);
+  myChart.setAxisPaddingLeft(50);
   myChart.setAxisPaddingBottom(40);
 //myChart.setTextPaddingLeft(105);
 //myChart.setTitleFontSize(11);
 //myChart.setBarBorderWidth(1);
   myChart.setBarBorderColor('#C4C4C4');
-  myChart.setBarSpacingRatio(50);
+  myChart.setBarSpacingRatio(40);
+  if(type == 'bar')
+  {
+    myChart.setBarColor('#88FF88', 3)
+    myChart.setBarColor('#FF8888', 2);
+    myChart.setLegendForBar(1, 'Total');
+    myChart.setLegendForBar(2, 'normal tariff');
+    myChart.setLegendForBar(3, 'night & we');
+    myChart.setLegendShow(true);
+    myChart.setLegendPosition('top middle');
+  }
 //myChart.setGrid(false);
-  myChart.setSize(616, 321);
+  myChart.setSize(800, 400);
 //myChart.setBackgroundImage('chart_bg.jpg');
   myChart.setBackgroundColor('#222244');
   myChart.setTooltipPosition('nw');
 
   var len=myData.length;
   for(var i=0; i<len; i++)
-    myChart.setTooltip([myData[i][0], myData[i][1]]);
+//    myChart.setTooltip([myData[i][0], myData[i][1]]);
+    myChart.setTooltip([myData[i][0]]);
+
+  myChart.draw();
+}
+
+function draw_we_chart(title, axis_x_name)
+{
+  <?php echo var_to_js('myData', $wedata); ?>
+
+  var myChart = new JSChart('wegraph', 'pie');
+  var colors = ['#FF8888','#88FF88'];
+  myChart.setDataArray(myData);
+  myChart.colorizePie(colors);
+  myChart.setTitle(title);
+  myChart.setTitleColor('#FFFFFF');
+  myChart.setSize(800, 200);
+  myChart.setBackgroundColor('#222244');
+  myChart.setLegend('#FF8888', 'normal tariff');
+  myChart.setLegend('#88FF88', 'night & week-end');
+  myChart.setPieRadius(95);
+  myChart.setShowXValues(false);
+  myChart.setLegendShow(true);
+  myChart.setLegendFontFamily('Times New Roman');
+  myChart.setLegendFontSize(10);
+  myChart.setLegendPosition(550, 80);
+  myChart.setPieValuesColor('#000000');
+  myChart.setPieAngle(50);
+  myChart.set3D(true);
 
   myChart.draw();
 }
@@ -237,8 +368,15 @@ echo "<div id=\"div3_example\"
 echo "<div id=\"graph\">";
 if(!$data)
   echo "No data for \"$title\"";
-else
+else{
   echo "<script language=javascript>draw_chart('$graph_type', '$title','$axis_x_name')</script></div>";
+}
+if($wedata)
+{
+  echo "<div id=\"wegraph\">";
+  echo "<script language=javascript>draw_we_chart('$title','$axis_x_name')</script></div>";
+}
+
 ?>
 </body>
 </html>
